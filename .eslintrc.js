ab02e4da2798b55935d00102566394269ffa2019
Wesley Ma <wesleykhma@gmail.com>
2019-03-03 16:47:14 -0500

    added linting script

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
+  extends: 'eslint:recommended',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: ['react'],
+  rules: {
+    indent: ['error', 2],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    'no-console': 'off',
+    'react/jsx-uses-react': 1,
+    'react/jsx-uses-vars': 1,
+    // 'react/react-in-jsx-scope': 1
+  }
+}
