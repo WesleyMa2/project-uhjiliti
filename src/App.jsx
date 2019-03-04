5ca17406140a3b5e02b53b8ba56a929031ffc061
Wesley Ma <wesleykhma@gmail.com>
2019-03-03 19:30:02 -0500

    base for main view

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,51 @@
-import React, { Component } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { Fragment } from 'react'
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
+import Paper from '@material-ui/core/Paper'
+import Tabs from '@material-ui/core/Tabs'
+import Tab from '@material-ui/core/Tab'
+import AppBar from '@material-ui/core/AppBar'
 
-class App extends Component {
+function Chat() {
+  return <h2>Chat</h2>
+}
+
+function Board() {
+  return <h2>Board</h2>
+}
+
+const styles = {}
+
+class App extends React.Component {
+  state = {
+    value: 0
+  }
+
+  handleChange = (event, value) => {
+    this.setState({ value })
+  }
+
   render() {
+    const { classes } = this.props
+    const { value } = this.state
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-						Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-						Learn React
-          </a>
-        </header>
+      <div>
+        <div className={classes.root}>
+          <AppBar position="static">
+            <Tabs centered value={value} onChange={this.handleChange}>
+              <Tab label="Chat" component={Link} to="/chat" />
+              <Tab label="Board" component={Link} to="/board" />
+            </Tabs>
+          </AppBar>
+        </div>
+        <Switch>
+          <Route path="/chat" exact component={Chat} />
+          <Route path="/board" component={Board} />
+        </Switch>
       </div>
     )
   }
 }
 
-export default App
+export default withStyles(styles)(App)
