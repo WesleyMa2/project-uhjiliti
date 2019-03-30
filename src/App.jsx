421e8b52855e97b0befb26fb1e59f11facfd339c
Wesley Ma <wesleykhma@gmail.com>
2019-03-30 00:40:02 -0400

    app now opens correct project based off url

--- src/App.jsx
+++ src/App.jsx
@@ -39,12 +39,17 @@ class App extends React.Component {
     this.setCurrProject = this.setCurrProject.bind(this)
     this.signout = this.signout.bind(this)
     this.newProject = this.newProject.bind(this)
-
+  }
+  // Sets the correct highlighted tab on load
+  componentDidMount() {
+    let currPath = window.location.pathname
+    if (currPath.split('/')[3] === 'chat') this.setState({ selectedTab: 5 })
+    else this.setState({ selectedTab: 4 })
   }
 
   // Set the current project
   setCurrProject(value) {
-    this.setState({currentProject: value})
+    this.setState({ currentProject: value })
     const url = history.location.pathname.split('/')
     url[2] = value
     const newUrl = url.join('/')
@@ -56,12 +61,6 @@ class App extends React.Component {
   // Changes the highlighted tab
   handleChange(event, value) {
     this.setState({ selectedTab: value })
-  }
-  // Sets the correct highlighted tab on load
-  componentDidMount() {
-    let currPath = window.location.pathname    
-    if (currPath.split('/')[3] === 'chat') this.setState({ selectedTab : 5 })
-    else this.setState({ selectedTab: 4 }) 
   }
 
   newProject(event) {
@@ -80,23 +79,31 @@ class App extends React.Component {
     return (
       <div>
         <AppBar position="fixed">
-          <Tabs value={selectedTab} onChange={this.handleChange} >
+          <Tabs value={selectedTab} onChange={this.handleChange}>
             <Typography style={styles.title} variant="h5" color="inherit" noWrap>
               Uhjiliti
             </Typography>
-            <ProjectMenu onSelect={this.setCurrProject}/>
-            <IconButton color="inherit"><Info/></IconButton>
-            <IconButton color="inherit"><PersonAdd/></IconButton>            
+            <ProjectMenu onSelect={this.setCurrProject} />
+            <IconButton color="inherit">
+              <Info />
+            </IconButton>
+            <IconButton color="inherit">
+              <PersonAdd />
+            </IconButton>
             <Tab label="Board" style={styles.centerTabs} component={Link} to={`/project/${this.state.currentProject}/board`} />
-            <Tab label="Chat" style={styles.centerTabs}component={Link} to={`/project/${this.state.currentProject}/chat`} />
-            <div style={{ flexGrow: 1 }}></div>
-            <IconButton onClick={this.newProject} color="inherit"><CreateNewFolder/></IconButton>
-            <IconButton onClick={this.signout} color="inherit"><ExitToApp/></IconButton>
+            <Tab label="Chat" style={styles.centerTabs} component={Link} to={`/project/${this.state.currentProject}/chat`} />
+            <div style={{ flexGrow: 1 }} />
+            <IconButton onClick={this.newProject} color="inherit">
+              <CreateNewFolder />
+            </IconButton>
+            <IconButton onClick={this.signout} color="inherit">
+              <ExitToApp />
+            </IconButton>
           </Tabs>
         </AppBar>
         <div id="app-container" ref="board">
-          <Route path={'/project/*/board'} render={() => <Board projectId={this.state.currentProject}/>} />
-          <Route path={'/project/*/chat'} exact render={()=> <Chat currentProject={this.state.currentProject}/>} />
+          <Route path={'/project/*/board'} render={() => <Board projectId={this.state.currentProject} />} />
+          <Route path={'/project/*/chat'} exact render={() => <Chat currentProject={this.state.currentProject} />} />
         </div>
       </div>
     )
--- src/Components/ProjectMenu.jsx
+++ src/Components/ProjectMenu.jsx
@@ -34,7 +34,11 @@ class ProjectMenu extends React.Component {
       .then(res => {
         if (res.data.projects.length > 0) {
           this.setState({ projects: res.data.projects })
-          this.props.onSelect(this.state.projects[0])
+          const urlProject = window.location.pathname.split('/')[2]
+          let projectIndex = this.state.projects.findIndex(el => el == urlProject)
+          if (projectIndex === -1) projectIndex = 0
+          this.setState({selectedIndex: projectIndex})
+          this.props.onSelect(this.state.projects[projectIndex])
         }
       })
       .catch(err => {
